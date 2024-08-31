48c0406fb5798ef8999d8012aa8c78e50e4f73f5
Andrey Koval <akoval29@gmail.com>
2024-08-31 20:00:28 +0300

    deploy-

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./js/script.js",
+  entry: './js/script.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -12,28 +13,31 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
-  devServer: {
-    static: "./dist",
-  },
-  mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',  // Вказує на ваш існуючий файл index.html
+      filename: 'index.html',   // Ім'я файлу, що буде створено в dist
+    }),
+  ],
+  mode: 'development',
 };
